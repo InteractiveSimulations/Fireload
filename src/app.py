e32f95dc0f63e6b604403cab86e4f0dbaae21493
sstein <steffen-sascha.stein@smail.th-koeln.de>
2022-05-19 13:11:46 +0200

    websocket implementation: basics and planning

--- src/app.py
+++ src/app.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
-import asyncio
-import python Blender API
 import websockets
-import itertools
 import json
-from connect4 import PLAYER1, PLAYER2, Connect4
+import blender
 
 
 # coroutine with the connection as argument.
@@ -14,71 +11,17 @@ async def handler(websocket):
 
     print("New client connected.")
 
-    # run blender
-    game = Connect4()
-
-
-    print("New game created.")
-
-    # Players take alternate turns, using the same browser.
-    turns = itertools.cycle([PLAYER1, PLAYER2])
-    player = next(turns)
+    # create Application instance?!
 
     async for message in websocket:
 
+        # 'Okay' vom Client, die Simulation zu beginnen -> Szene ist final
 
+        # JSON 'Drehbuch' abfangen
+        # Prototype: Nur Startposition des Feuers
+        sceneInformation = json.loads(message)
 
-        # Parse a "play" event from the UI.
-        event = json.loads(message)
-        assert event["type"] == "play"
-        column = event["column"]
-
-        # Play the move
-        try:
-            row = game.play(player, column)
-        except RuntimeError as exc:
-            # Send an "error" event if the move was illegal.
-            event = {
-                "type": "error",
-                "message": str(exc)
-            }
-            await websocket.send(json.dumps(event))
-            continue
-
-        # Send a "play" event to update the UI.
-        event = {
-            "type": "play",
-            "player": player,
-            "column": column,
-            "row": row
-        }
-        await websocket.send(json.dumps(event))
-
-        # If move is wining, send a "win" event.
-        if game.winner is not None:
-            event = {
-                "type": "win",
-                "player": game.winner
-            }
-            await websocket.send(json.dumps(event))
-
-        print(message)
-        print(event)
-
-        # Alternate turns.
-        player = next(turns)
-
-    # async for message in websocket:
-    #     print(message)
-    #
-    # # The code above is a shortcut for this behaviour:
-    #
-    # while True:
-    #     try:
-    #         message = await websocket.recv()
-    #     except websockets.ConnectionClosedOK:
-    #         break
-    #     print(message)
+        blender.startBlender(sceneInformation)
 
 
 # coroutine which waits for a client to connect.
---
+++ src/blender.py
@@ -0,0 +1,4 @@
+
+# Funktion startet Blender und übergibt das Drehbuch
+def startBlender(drehbuch):
+    print(drehbuch)
