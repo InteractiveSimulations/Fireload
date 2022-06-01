0bc36755944e3fb90de1691e5b65c84f410b9329
forEachWhileTrue <steffen-sascha.stein@smail.th-koeln.de>
2022-06-01 12:51:06 +0200

    added websocket functionality

--- ThreeClient/src/js/websocket.js
+++ ThreeClient/src/js/websocket.js
@@ -5,14 +5,29 @@ function showMessage(message) {
 }
 
 function receiveSimulation( data ) {
-        const event = JSON.parse(data);
+    const event = JSON.parse(data);
 }
 
-function startSimulation( json ) {
-    // When clicking a column, send a "play" event for a move in that column.
+/**
+ * Requests a simulation.
+ * @param {string} json - json string which represents the scenes current state and the simulation script.
+ */
+function requestSimulation( json ) {
 
-    websocket.send(json);
+    try {
+        websocket.send(json);
+    }
+    catch (err) {
+        // @Todo inform client via GUI that the connection isn't established
+        // InvalidStateError DOMException
+        // Thrown if WebSocket.readyState is CONNECTING.
+        showMessage( "websocket connection not established.")
+    }
+
 }
+
+// sessionHandling or better inform the client, that the server closed the connection
+function sessionHandling() {}
 
 window.addEventListener("DOMContentLoaded", () => {
 
@@ -20,6 +35,25 @@ window.addEventListener("DOMContentLoaded", () => {
     websocket = new WebSocket("ws://localhost:8001/");
     receiveSimulation();
 
-    websocket.addEventListener("message", receiveSimulation );
+    websocket.addEventListener( "message",  receiveSimulation );
+    websocket.addEventListener( "close",    sessionHandling   );
+
+    // websocket.onmessage = receiveSimulation
+    // websocket.onclose   = sessionHandling
 
 });
+
+// Each WebSocket object has an associated ready state, which is a number representing the state of the connection.
+// Initially it must be CONNECTING (0). It can have the following values:
+//
+// CONNECTING (numeric value 0)
+// The connection has not yet been established.
+//
+// OPEN (numeric value 1)
+// The WebSocket connection is established and communication is possible.
+//
+// CLOSING (numeric value 2)
+// The connection is going through the closing handshake, or the close() method has been invoked.
+//
+// CLOSED (numeric value 3)
+// The connection has been closed or could not be opened.
